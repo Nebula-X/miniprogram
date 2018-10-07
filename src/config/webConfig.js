796496a3cfe3a16bf8d54f319af4b726a0f71786
Nebula-X <zhaojt_wechat@126.com>
2018-10-07 22:34:10 +0800

    提交网站URL

--- src/config/webConfig.js
+++ src/config/webConfig.js
@@ -1,4 +1,4 @@
 // API -- 访问地址
-export const BaseURL = 'https://apiapiapi.pcbbao.com/remote-api/index.php/'
+export const BaseURL = 'https://.com/remote-api/index.php/'
 // API -- 正式地址
-// export const BaseURL = 'http://apiapiapi.pcbbao.com/remote-api/index.php/'
+// export const BaseURL = 'http://.com/remote-api/index.php/'
